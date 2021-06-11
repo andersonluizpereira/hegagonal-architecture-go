43524df578817c393ba0761709fe2b2c915eb8c6
Anderson Pereira <andersononus@hotmail.com>
2021-06-11 20:08:34 -0300

    test product service error get

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,6 +1,7 @@
 package application_test
 
 import (
+	"errors"
 	"github.com/acpereira/go-hexagonal/application"
 	mock_application "github.com/acpereira/go-hexagonal/application/mocks"
 	"github.com/golang/mock/gomock"
@@ -22,5 +23,22 @@ func TestProductService_Get(t *testing.T) {
 	result, err := service.Get("123")
 	require.Nil(t, err)
 	require.Equal(t, product, result)
+
+}
+
+func TestProductService_Get_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, errors.New("ID not found")).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+	result, err := service.Get("123")
+	require.Equal(t, "ID not found",err.Error())
+	require.Equal(t, nil, result)
 
 }

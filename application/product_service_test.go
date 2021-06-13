3082d42db0edd5657b5b4fff31308c590ef83ea0
Anderson Pereira <andersononus@hotmail.com>
2021-06-13 19:27:11 -0300

    test service method disable and enable with errors

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -106,6 +106,34 @@ func TestProductService_Enable(t *testing.T) {
 	require.Equal(t, product, result)
 }
 
+func TestProductService_Enable_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(errors.New("the price must be greater than zero to enable the product")).AnyTimes()
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence,}
+
+	_, err := service.Enable(product)
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProductService_Enable_Error_Save(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil).AnyTimes()
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, errors.New("Server Error")).AnyTimes()
+	service := application.ProductService{Persistence: persistence,}
+
+	_, err := service.Enable(product)
+	require.Equal(t, "Server Error", err.Error())
+}
+
 func TestProductService_Disable(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -119,4 +147,32 @@ func TestProductService_Disable(t *testing.T) {
 	result, err := service.Disable(product)
 	require.Nil(t, err)
 	require.Equal(t, product, result)
+}
+
+func TestProductService_Disable_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(errors.New("the price must be greater than zero to enable the product")).AnyTimes()
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence,}
+
+	_, err := service.Disable(product)
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProductService_Disable_Error_Save(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(nil).AnyTimes()
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, errors.New("Server Error")).AnyTimes()
+	service := application.ProductService{Persistence: persistence,}
+
+	_, err := service.Disable(product)
+	require.Equal(t, "Server Error", err.Error())
 }
